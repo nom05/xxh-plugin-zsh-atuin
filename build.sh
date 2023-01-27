a6596bd7ae9c0064a8274394edafd4711ee4274d
nom05 <70644469+nom05@users.noreply.github.com>
2023-01-28 00:16:32 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,22 +21,23 @@ do
     cp $CDIR/$f $build_dir/
 done
 
-#portable_url='https://,,,/.tar.gz'
-#tarname=`basename $portable_url`
-#
-#cd $build_dir
-#
-#[ $QUIET ] && arg_q='-q' || arg_q=''
-#[ $QUIET ] && arg_s='-s' || arg_s=''
-#[ $QUIET ] && arg_progress='' || arg_progress='--show-progress'
-#
-#if [ -x "$(command -v wget)" ]; then
-#  wget $arg_q $arg_progress $portable_url -O $tarname
-#elif [ -x "$(command -v curl)" ]; then
-#  curl $arg_s -L $portable_url -o $tarname
-#else
-#  echo Install wget or curl
-#fi
-#
-#tar -xzf $tarname
-#rm $tarname
+portable_url='https://github.com/ellie/atuin/releases/download/v12.0.0/atuin-v12.0.0-x86_64-unknown-linux-gnu.tar.gz'
+tarname=`basename $portable_url`
+
+cd $build_dir
+
+[ $QUIET ] && arg_q='-q' || arg_q=''
+[ $QUIET ] && arg_s='-s' || arg_s=''
+[ $QUIET ] && arg_progress='' || arg_progress='--show-progress'
+
+if [ -x "$(command -v wget)" ]; then
+  wget $arg_q $arg_progress $portable_url -O $tarname
+elif [ -x "$(command -v curl)" ]; then
+  curl $arg_s -L $portable_url -o $tarname
+else
+  echo Install wget or curl
+fi
+
+tar -xzf $tarname
+mv ${tarname/.tar.gz} bin
+rm $tarname
